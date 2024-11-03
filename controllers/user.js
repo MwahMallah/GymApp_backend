d35c0fb3e533b1f58f9169a2c356893b01568688
MwahMallah <maximus27rus@gmail.com>
2024-11-03 13:43:32 +0100

    User can add friends.

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const usersRouter = require('express').Router();
+const middleware = require('../utils/middleware');
 const User = require('../models/user');
 const bcrypt = require('bcrypt')
 
@@ -21,7 +22,8 @@ usersRouter.get('/:id', async (req, res) => {
     const id = req.params.id;
     const users = await User.findById(id)
         .populate('exercises', {user: 0})
-        .populate('food', {user: 0});
+        .populate('food', {user: 0})
+        .populate('friends');
 
     res.json(users);
 });
@@ -45,6 +47,41 @@ usersRouter.post('/', async (req, res, next) => {
     } catch(e) {
 
         next(e);
+    }
+});
+
+//add friend
+usersRouter.post('/friends/:id', middleware.userExtractor, async (req, res, next) => {
+    
+    const user = req.user;
+    const friendId = req.params.id;
+
+    try {
+        // User shouldn't add himself
+        if (user.id === friendId) {
+            return res.status(400).json({ error: "You can't add yourself as a friend." });
+        }
+
+        const friend = await User.findById(friendId);
+
+        if (!friend) {
+            return res.status(404).json({ error: "User not found." });
+        }
+
+        // Check that friend is not already added
+        if (user.friends.includes(friendId)) {
+            return res.status(400).json({ error: "Friend already added." });
+        }
+
+        // Adding friend
+        user.friends.push(friendId);
+        friend.friends.push(user.id);
+        await user.save();
+        await friend.save();
+
+        res.status(204).end();
+    } catch (error) {
+        next(error);
     }
 });
 
--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
+const default_photo_url = "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3408.jpg?semt=ais_hybrid";
+
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, minLength: 3, unique: true},
     name: {type: String},
     exercises: [{type: mongoose.Schema.Types.ObjectId, ref: "Exercise"}],
     food: [{type: mongoose.Schema.Types.ObjectId, ref: "Food"}],
+    friends: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    photo_url: {type: String, default: default_photo_url},
     passwordHash: {type: String},
 });
 
